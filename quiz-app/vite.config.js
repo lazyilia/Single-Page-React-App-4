c4d86954dc82b133e2b1c38e1ac7e3147f30694a
lazyilia <ilia.mirzaei2006@gmail.com>
2025-07-24 15:41:40 +0330

    updated

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : '/Single-Page-React-App-4/'
+  base: '/Single-Page-React-App-4/',
 })
